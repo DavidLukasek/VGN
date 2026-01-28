6b1321a735bca86db17aee7f825c23f144efb357
David Lukášek <lukasda7@fel.cvut.cz>
2026-01-28 22:53:05 +0100

    Added ingame help, polished menus

---
+++ VGN/Assets/Scripts/HelpManager.cs
@@ -0,0 +1,31 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HelpManager : MonoBehaviour
+{
+    public TMP_Text helpText;
+    public string originalText;
+    public string newText;
+
+    private bool isOriginalText;
+
+    void Start()
+    {
+        isOriginalText = true;
+    }
+
+    public void OnHelp(InputValue value)
+    {
+        if(isOriginalText)
+        {
+            helpText.text = newText;
+            isOriginalText = false;
+        }
+        else
+        {
+            helpText.text = originalText;
+            isOriginalText = true;
+        }
+    }
+}
